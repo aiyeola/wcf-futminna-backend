1b1bdce503950e2b87be90bfa217d659e2b95f16
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-06-15 14:39:07 +0100

    chore: db seed data

--- src/database/seed/index.js
+++ src/database/seed/index.js
@@ -1,15 +1,17 @@
 import 'dotenv/config';
 import seeder from 'mongoose-seed';
 
+import sampleData from 'database/seed/sampleData';
+
 // Data array containing seed data - documents organized by Model
 const data = [
   {
     model: 'Admin',
     documents: [
       {
-        username: 'president',
+        username: 'admin',
         password:
-          '$2b$10$vewFOuFlpa9c5wgkaxONlu/x9R3E4XqDR49sZH5T0LR7bjZ8xeFfq',
+          '$argon2i$v=19$m=4096,t=3,p=1$vzrWD7gGrd3WGy4EaJu6Bg$brMu6j3y1PH05NEQr8lfjyx0GoUsUrU7WzcqIRgAlbY',
         userRole: 'Super Administrator',
         isAdmin: true,
       },
@@ -17,39 +19,7 @@ const data = [
   },
   {
     model: 'BioData',
-    documents: [
-      {
-        firstName: 'Jane',
-        lastName: 'Doe',
-        department: 'Chemical Engineering',
-        email: 'janedoe@livemail.com',
-        schoolAddress: 'Estate Arizona',
-        homeAddress: 'Block E, Livelyhood estate',
-        level: 500,
-        contactNumber1: 8112891128,
-        contactNumber2: 8126794444,
-        unit: 'House of Honour',
-        dob: '1999-05-29',
-        origin: 'Rivers',
-        gender: 'Male',
-        campus: 'Gidan-Kwano',
-      },
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        department: 'Mathematics',
-        email: 'doe.john@hotmail.com',
-        schoolAddress: 'Unity Villa',
-        homeAddress: 'Block A, Rail road',
-        level: 400,
-        contactNumber1: 8146724733,
-        unit: 'Drama',
-        dob: '1996-05-29',
-        origin: 'Gombe',
-        gender: 'Female',
-        campus: 'Bosso',
-      },
-    ],
+    documents: sampleData,
   },
 ];
 
@@ -61,7 +31,7 @@ seeder.connect(dbUri, { useUnifiedTopology: true }, () => {
   ]);
 
   // Clear specified collections
-  seeder.clearModels(['Admin', 'BioForm'], () => {
+  seeder.clearModels(['Admin', 'BioData'], () => {
     // Callback to populate DB once collections have been cleared
     seeder.populateModels(data, (err, done) => {
       seeder.disconnect();
